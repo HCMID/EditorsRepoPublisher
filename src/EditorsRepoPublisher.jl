a6916fecd5532d8c95a3e772e9618bfc28c91651
Neel Smith <dnsmith.neel@gmail.com>
2021-02-23 17:52:23 -0500

    Start to outline project

--- src/EditorsRepoPublisher.jl
+++ src/EditorsRepoPublisher.jl
@@ -1,5 +1,22 @@
 module EditorsRepoPublisher
 
-greet() = print("Hello World!")
+using EditorsRepo
+using CSV
+
+export publish
+
+
+function publish(catalogrow::CSV.Row, textroot)
+
+end
+
+"""Publish all online texts in repository.
+"""
+function publish(repo::EditingRepository, textroot::AbstractString)
+    publishable = EditorsRepo.online(repo, "catalog.cex")
+    for txt in publishable
+        publish(txt, textroot)
+    end
+end
 
 end # module
